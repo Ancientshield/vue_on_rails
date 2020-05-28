ac1d48f02399a4e8519fea7006a7da1177ae36f2
Ancientsword <jacklin2100@gmail.com>
2020-05-28 18:13:50 +0800

    add vue template

---
+++ app/javascript/components/Message.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="message">{{message}}</div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      message: "Vue card"
+    };
+  }
+};
+</script>
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,11 +2,3 @@ require("@rails/ujs").start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
-
-import Vue from "vue/dist/vue.esm";
-
-document.addEventListener("DOMContentLoaded", () => {
-  const app = new Vue({
-    el: '[data-behavior="vue-app"]',
-  });
-});
