0fb48d68d485284886104750b340700d0501bc06
Ancientsword <jacklin2100@gmail.com>
2020-05-30 08:53:42 +0800

    add turbolinks in main.js

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -1,11 +1,10 @@
 import Vue from "vue";
 import Vuetify from "vuetify";
-import "../src/application.scss";
 import App from "../app.vue";
 
 Vue.use(Vuetify);
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("turbolinks:load", () => {
   const app = new Vue({
     vuetify: new Vuetify(),
     render: (h) => h(App),
