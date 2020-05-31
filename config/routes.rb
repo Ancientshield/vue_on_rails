a9be362b661db8c7fcad7fcfb969480e7aa6b049
Ancientsword <jacklin2100@gmail.com>
2020-05-31 22:32:37 +0800

    create users_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/new'
+  get 'user/index'
+  get 'user/new'
   get 'home/index'
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
