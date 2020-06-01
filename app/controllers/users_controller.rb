f5fd9186e1f6b896b2da0d1d95028ae0a97d8602
Ancientsword <jacklin2100@gmail.com>
2020-06-01 14:15:33 +0800

    add sessions controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,23 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
-  def index
+  def index; end
+
+  def new; end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      redirect_to '/signup'
+    end
   end
 
-  def new
+  private
+
+  def user_params
+    params.permit(:name, :email, :password, :password_confirmation)
   end
 end
