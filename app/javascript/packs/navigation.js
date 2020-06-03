88c5c461c06c15681a4e30d0e76087e66d07affa
Ancientsword <jacklin2100@gmail.com>
2020-06-03 15:08:58 +0800

    modify view

--- app/javascript/packs/navigation.js
+++ app/javascript/packs/navigation.js
@@ -6,3 +6,11 @@
 //     el: '[data-behavior="vue-app"]',
 //   });
 // });
+<v-container>
+  <v-row>
+    <Navigation></Navigation>
+  </v-row>
+  <v-row>
+    <v-user></v-user>
+  </v-row>
+</v-container>;
