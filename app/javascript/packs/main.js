316071905abf7229d4c37b1cd64584cf4b52e1d4
Ancientsword <jacklin2100@gmail.com>
2020-06-03 17:42:00 +0800

    resolve heroku

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -1,8 +1,8 @@
 import Vue from "vue";
 import Vuetify from "vuetify";
-import Layout from "@/components/Layout";
-import User from "@/components/User";
-import Navigation from "@/components/Navigation";
+import Layout from "./components/Layout";
+// import User from "./components/User";
+// import Navigation from "./components/Navigation";
 
 Vue.use(Vuetify);
 
@@ -12,8 +12,6 @@ document.addEventListener("turbolinks:load", () => {
     vuetify: new Vuetify(),
     components: {
       Layout,
-      User,
-      Navigation,
     },
   });
 });
