ea8cd37e85c555f1eaa7b4b8f8e205ca8a7fdbb6
Ancientsword <jacklin2100@gmail.com>
2020-06-04 10:29:38 +0800

    restore Module not found: Error

--- config/webpack/custom.js
+++ config/webpack/custom.js
@@ -23,4 +23,10 @@ module.exports = {
       },
     ],
   },
+  resolve: {
+    alias: {
+      vue$: "vue/dist/vue.esm.js",
+    },
+    extensions: ["*", ".js", ".vue", ".json"],
+  },
 };
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,14 +5,14 @@ const vue = require("./loaders/vue");
 environment.plugins.prepend("VueLoaderPlugin", new VueLoaderPlugin());
 environment.loaders.prepend("vue", vue);
 
-const resolver = {
-  resolve: {
-    alias: {
-      vue$: "vue/dist/vue.esm.js",
-    },
-    extensions: ["*", ".js", ".vue", ".json"],
-  },
-};
-environment.config.merge(resolver);
+// const resolver = {
+//   resolve: {
+//     alias: {
+//       vue$: "vue/dist/vue.esm.js",
+//     },
+//     extensions: ["*", ".js", ".vue", ".json"],
+//   },
+// };
+// environment.config.merge(resolver);
 
 module.exports = environment;
