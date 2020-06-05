3daf9134ce0c2182d4d5af5abcc10392dc4f539c
Ancientsword <jacklin2100@gmail.com>
2020-06-05 21:20:57 +0800

    change nav bar to vuetify Breadcrumbs

--- app/javascript/packs/components/navigation.vue
+++ app/javascript/packs/components/navigation.vue
@@ -1,38 +1,29 @@
 <template>
-  <div class="navigation-bar">
-    <a class="navigation-item" :href="link.path">{{ link.text }}</a>
+  <div>
+    <v-breadcrumbs :items="items" large></v-breadcrumbs>
   </div>
 </template>
 
 <script>
 export default {
-  // This is data you can access in the template
-  // It's a standard hash of data in key:value pairs
-  data: function() {
-    return {
-      link: {
-        path: "/",
-        text: "Home",
-        id: 1,
+  data: () => ({
+    items: [
+      {
+        text: "Dashboard",
+        disabled: false,
+        href: "#"
       },
-    };
-  },
+      {
+        text: "Link 1",
+        disabled: false,
+        href: "#"
+      },
+      {
+        text: "Link 2",
+        disabled: true,
+        href: "#"
+      }
+    ]
+  })
 };
 </script>
-
-<style scoped>
-/* This css applies *only* to this component */
-.navigation-bar {
-  display: flex;
-  justify-content: space-around;
-  width: 100%;
-  background-color: #198dbb;
-  padding: 10px 0;
-}
-
-.navigation-item {
-  font-family: Arial, Helvetica, sans-serif;
-  color: white;
-  line-height: 1.6;
-}
-</style>
